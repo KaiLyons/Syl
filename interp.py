1bf8833a7e54d11e177c6e823fc2a8fc4d26741e
Safyre <lyons.safyre@gmail.com>
2019-08-12 06:29:57 -0600

    Added the ability to check for : properly

--- interp.py
+++ interp.py
@@ -6,29 +6,35 @@ contents = app.read()
 def read():
     e = contents.strip()
     egg = e.startswith("int _main()")
+    rt = e.split("\n")[0]
+    rtt = e.split("\n")[0].endswith(":")
     if(egg):
-        eg = e.split(":" + "\n")[1]
-        er = eg.startswith(" ")
-        egg2 = e.endswith(";")
-        y = e.split("print")[1]
-        r = y.split("\");")[0]
-        g = r.split("(\"")[1]
-        def prfn():
-            if(er):
-                if(y):
-                    if(g):
-                        print(g)
+        if(not(rtt)):
+            print("No : after " + rt)
+        elif(rtt):
+            egr = e.split(":" + "\n")[1]
+            er = egr.startswith(" ")
+            egg2 = e.endswith(";")
+            y = e.split("print")[1]
+            r = y.split("\");")[0]
+            g = r.split("(\"")[1]
+            def prfn():
+                if(er):
+                    if(y):
+                        if(g):
+                            print(g)
+                        else:
+                            print("failed to find string area")
                     else:
                         print("failed to find string area")
                 else:
-                    print("failed to find string area")
+                    print("no indent found after new line")
+            if(egg2):
+                prfn()
             else:
-                print("no indent found after new line")
+                print("missing ;")
 
-        if(egg2):
-            prfn()
-        else:
-            print("missing ;")
+        
     else:
         print("no detection of main int")
 
